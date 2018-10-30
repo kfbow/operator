b54a66d3b8e7cd8a1cc05de7e9c11f97fa7455e7
Kolbe <me@kolbe.io>
2018-10-29 19:06:29 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,31 @@
+export default function operator() {
+        const operator = {};
+	const timing = performance.getEntriesByType("navigation")[0];
+
+	///////
+	// Page Load in ms
+	// Returns a promise that when completed will send the page load time
+	//
+	// Created by subtracting the navigation start from the end of the
+	// load events
+	// https://developer.mozilla.org/en-US/docs/Web/API/Navigation_timing_API#Calculate_the_total_page_load_time
+	//////
+	operator.pageLoad = () => {
+		var fullLoad = timing.loadEventEnd - timing.fetchStart;
+		var promise = new Promise(function cb(resolve, reject) {
+			if (fullLoad < 0){
+     				window.setTimeout(function() {
+					fullLoad = timing.loadEventEnd - timing.fetchStart;
+					cb(resolve, reject);
+     				}, 100);
+			} else {
+				resolve(fullLoad);
+			}
+   		});
+   		return promise;
+	}
+
+	window.op = operator;
+
+        return operator;
+}
